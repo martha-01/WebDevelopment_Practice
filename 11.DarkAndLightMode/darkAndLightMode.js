c84c89f24517419be000bd89992c35367ff3fb83
Márta Kapocs <marta.kapocs@gmail.com>
2023-11-04 15:11:10 +0100

    uploading exercises 8, 9, 10 and 11

---
+++ 11.DarkAndLightMode/darkAndLightMode.js
@@ -0,0 +1,9 @@
+function switchModes() {
+    if (document.body.className == "dark-theme") {
+        document.body.className = "light-theme";
+        document.getElementById("btn").innerText = "Dark";
+    } else {
+        document.body.className = "dark-theme";
+        document.getElementById("btn").innerText = "Light";
+    }
+}
